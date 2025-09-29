5b31d8467c8382ffe66b4fb10a45c8ca95b10149
Mohamed Osama Menisy <116950544+mohamedmenisy@users.noreply.github.com>
2025-09-29 15:25:19 +0300

    add login page , handel errors with toast in primeng , create toast service

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,11 +1,40 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import {  ILoginResponse } from '../../interfaces/login';
+import { ToastService } from '../../../../core/services/toast.service';
 
 @Component({
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  
+  PasswordPattern: RegExp =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{1,10}$/;
+  passwordError: string =
+    'Password must contain uppercase, lowercase, number, symbol (max 10 chars)';
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(this.PasswordPattern),
+    ]),
+  });
+  constructor(private _auth: AuthService, private _toast: ToastService,private router:Router) {}
+  SubmitForm(data: FormGroup) {
+    this._auth.login(data.value).subscribe({
+      next: (res: ILoginResponse) => {
+        this._toast.showSuccess('login success');
+        localStorage.setItem('token',res.data.accessToken);
+
+      },
+      complete:()=>{
+        //route to dashboard
+        this.router.navigate(['/auth'])
+      }
+    });
+  }
 }
