67b31e77517d346ead2d44ed86af02b92dd70ee2
Your Name <your-email@example.com>
2025-09-30 14:23:45 +0300

    add change-password

--- src/app/features/auth/components/change-password/change-password.component.ts
+++ src/app/features/auth/components/change-password/change-password.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import {  ILoginResponse } from '../../interfaces/login';
+import { ToastService } from '../../../../core/services/toast.service';
 @Component({
   selector: 'app-change-password',
   standalone: false,
@@ -7,5 +12,31 @@ import { Component } from '@angular/core';
   styleUrl: './change-password.component.scss'
 })
 export class ChangePasswordComponent {
+  PasswordPattern: RegExp =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{1,10}$/;
+  passwordError: string =
+    'Password must contain uppercase, lowercase, number, symbol (max 10 chars)';
+  loginForm: FormGroup = new FormGroup({
+    password_new: new FormControl(null, [Validators.required, Validators.pattern(this.PasswordPattern) ]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.pattern(this.PasswordPattern),
+    ]),
+  });
+  constructor(private _auth: AuthService, private _toast: ToastService,private router:Router) {}
+  SubmitForm(data: FormGroup) {
+    this._auth.ChangePassword(data.value).subscribe({
+      next: (res) => {
+       console.log(res);
+       
+        
 
+      },
+      complete:()=>{
+         this._toast.showSuccess('login success');
+        this.router.navigate(['/auth'])
+      }
+    });
+  }
 }
+
--- src/app/features/auth/interfaces/login.ts
+++ src/app/features/auth/interfaces/login.ts
@@ -22,3 +22,4 @@ export interface IProfile {
   status: string
   role: string
 }
+
