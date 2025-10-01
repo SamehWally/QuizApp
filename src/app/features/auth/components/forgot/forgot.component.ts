49256b76e0138436a28630ec36f41a839957f3c8
SamehWally <samehwaly27@gmail.com>
2025-10-01 18:59:45 +0300

    feat: complete register and forgot form with role dropdown and validation

--- src/app/features/auth/components/forgot/forgot.component.ts
+++ src/app/features/auth/components/forgot/forgot.component.ts
@@ -1,11 +1,55 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { ILoginResponse } from '../../interfaces/login';
+import { ToastService } from '../../../../core/services/toast.service';
+import { MyTranslateService } from '../../../../core/services/my-translate.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-forgot',
   standalone: false,
   templateUrl: './forgot.component.html',
-  styleUrl: './forgot.component.scss'
+  styleUrl: './forgot.component.scss',
 })
 export class ForgotComponent {
+  iconStyle: string = 'password-icon-ltr';
+  lang: string = 'en';
 
+  forgotForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+  });
+
+  constructor(
+    private _auth: AuthService,
+    private _toast: ToastService,
+    private router: Router,
+    private translate: TranslateService
+  ) {}
+
+  ngOnInit(): void {
+    this.translate.onLangChange.subscribe((event: any) => {
+      if (event.lang == 'ar') {
+        this.iconStyle = 'password-icon-rtl';
+        this.lang = 'ar';
+      } else {
+        this.iconStyle = 'password-icon-ltr';
+        this.lang = 'en';
+      }
+    });
+  }
+
+  SubmitForm(data: FormGroup) {
+    console.log(data.value);
+
+    this._auth.forgot(data.value).subscribe({
+      next: (res: any) => {
+        this._toast.showSuccess('success');
+      },
+      complete: () => {
+        this.router.navigate(['/auth/reset']);
+      },
+    });
+  }
 }
