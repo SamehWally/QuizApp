10bb362cc9ad3b66b3fe4dfede0ae98ef6206d8d
Your Name <your-email@example.com>
2025-10-05 20:47:56 +0300

    add translate

--- src/app/features/dashboard/instructor/components/results/components/completed-quizzes/completed-quizzes.component.ts
+++ src/app/features/dashboard/instructor/components/results/components/completed-quizzes/completed-quizzes.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,inject} from '@angular/core';
 import { ResuitsService } from '../../services/resuits.service';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from "../../../../../../auth/auth-routing.module";
+import { MyTranslateService } from '../../../../../../../core/services/my-translate.service';
+import { SharedModule } from '../../../../../../../Shared/shared.module';
+
 @Component({
   selector: 'app-completed-quizzes',
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule,SharedModule],
   templateUrl: './completed-quizzes.component.html',
   styleUrl: './completed-quizzes.component.scss'
 })
 export class CompletedQuizzesComponent implements OnInit {
-
+ private _MyTranslateService = inject(MyTranslateService);
   resuit!:any[];
 constructor(private _ResuitsService:ResuitsService){}
   ngOnInit(): void {
